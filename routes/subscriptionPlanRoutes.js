2ab3fed030341d804ea8906c8be40bee3116a359
Md Jasim <mdjasimm107@gmail.com>
2026-01-02 00:23:39 +0530

    Fix auth middleware import in subscription routes

--- routes/subscriptionPlanRoutes.js
+++ routes/subscriptionPlanRoutes.js
@@ -9,18 +9,18 @@ const {
   togglePlanStatus,
   deletePlan
 } = require('../controllers/subscriptionPlanController');
-const auth = require('../middleware/auth');
+const { protect } = require('../middleware/auth');
 const ownerOnly = require('../middleware/ownerOnly');
 
 // Public route - Get active plans (used during Add Customer)
 router.get('/', getSubscriptionPlans);
 
 // Protected routes - Owner only
-router.get('/all', auth, ownerOnly, getAllPlansForOwner);
-router.get('/:id', auth, ownerOnly, getPlanById);
-router.post('/', auth, ownerOnly, createPlan);
-router.put('/:id', auth, ownerOnly, updatePlan);
-router.patch('/:id/status', auth, ownerOnly, togglePlanStatus);
-router.delete('/:id', auth, ownerOnly, deletePlan);
+router.get('/all', protect, ownerOnly, getAllPlansForOwner);
+router.get('/:id', protect, ownerOnly, getPlanById);
+router.post('/', protect, ownerOnly, createPlan);
+router.put('/:id', protect, ownerOnly, updatePlan);
+router.patch('/:id/status', protect, ownerOnly, togglePlanStatus);
+router.delete('/:id', protect, ownerOnly, deletePlan);
 
 module.exports = router;
